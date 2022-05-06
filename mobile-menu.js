f6b916bd2405ba0fb1363adeea88ca570b26fae9
ksenksenni <nikolaevaksa@gmail.com>
2022-05-06 22:07:20 +0300

    fixed margins, paddings

--- mobile-menu.js
+++
@@ -1,7 +0,0 @@
-var mobileMenu = document.querySelector('.mobile-menu__container');
-var mobileBtn = document.querySelector('.mobile-menu');
-
-mobileBtn.addEventListener('click', function() {
-    mobileMenu.classList.toggle("shown");
-
-})
